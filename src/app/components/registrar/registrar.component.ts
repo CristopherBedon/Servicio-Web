42efab503fa5efa7f11ddf0684335402a77a9315
jeremychaflocgit <i201916552@cibertec.edu.pe>
2021-12-04 13:05:57 -0500

    Registro actualizado

--- src/app/components/registrar/registrar.component.ts
+++ src/app/components/registrar/registrar.component.ts
@@ -10,23 +10,36 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class RegistrarComponent implements OnInit {
 
-  usuario = {email:'',password:'',}
-  
+  usuario = { email: '', password: '', }
+
+  regForm: FormGroup = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.pattern('[a-zA-Z.0-9]+@[a-zA-Z0-9]+\.[a-z]{2,4}')]],
+    password: ['', [Validators.required, Validators.pattern('[0-9]{4,8}')]],
+  });
+
+  submitted = false;
+
   constructor(
-    private authService : AuthService,
-    private router: Router
-  ){ }
+    private authService: AuthService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
   }
 
-  registrar(){
-    const {email,password} = this.usuario;
-    this.authService.register(email,password).then(res => {
-      console.log("Se registro: " , res);
-    })
-    alert("Se registró el Usuario: " + email + "\n Por favor. Verifique su correo antes de iniciar sesión");
-    this.router.navigate(['/login']);
+  registrar() {
+    this.submitted = true;
+    if (this.regForm.invalid) {
+      return;
+    } else {
+      const { email, password } = this.usuario;
+      this.authService.register(email, password).then(res => {
+        console.log("Se registro: ", res);
+      });
+      alert("Se registró el Usuario: " + email + "\n Por favor. Verifique su correo antes de iniciar sesión");
+      this.router.navigate(['/login']);
+    }
   }
 
 }
